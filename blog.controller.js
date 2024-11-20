160892483b50bb314abc0577bfb0e7f2cadba2fb
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2024-11-20 12:01:20 +0100

    update put method to go to db isntead

--- blog.controller.js
+++ blog.controller.js
@@ -11,7 +11,29 @@ function getAllPosts(req, res) {
   return res.json(posts);
 }
 
-function postBlog(req, res) {
+function getPostById(req, res) {
+  //   const params = req.params;
+  //   const id = params.id;
+  const { id } = req.params;
+
+  const getPostByIdQuery = `
+    SELECT * FROM posts 
+    WHERE post_id = ?
+`;
+
+  const stmt = db.prepare(getPostByIdQuery);
+  const post = stmt.get([id]);
+
+  if (post) {
+    return res.json(post);
+  }
+
+  return res
+    .status(404) // Means NOT FOUND
+    .json({ message: "The post with that id was not found" });
+}
+
+function postPost(req, res) {
   const body = req.body;
   const lengthOfBody = Object.keys(body).length;
 
@@ -34,7 +56,45 @@ function postBlog(req, res) {
     .json({ message: "The new post was successfully created" });
 }
 
+function putPost(req, res) {
+  const { id } = req.params;
+  const { body } = req; // object destructuring
+
+  if (!body.content) {
+    return res
+      .status(400)
+      .json({ message: "body.content is malformed or doesn't exist." });
+  }
+
+  const getPostByIdQuery = `
+    SELECT * FROM posts 
+    WHERE post_id = ?
+`;
+
+  let stmt = db.prepare(getPostByIdQuery);
+  const post = stmt.get([id]);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ message: "The post with that id was not found" });
+  }
+
+  const putPostQuery = `
+    UPDATE posts
+    SET content = ?
+    WHERE post_id = ?
+  `;
+
+  stmt = db.prepare(putPostQuery);
+  stmt.run([body.content, id]);
+
+  return res.json({ message: "The post was successfully updated" });
+}
+
 module.exports = {
   getAllPosts,
-  postBlog,
+  getPostById,
+  postPost,
+  putPost,
 };
--- index.js
+++ index.js
@@ -4,7 +4,12 @@
 const express = require("express");
 
 // Import the controller methods from blog.controller.js
-const { getAllPosts, postBlog } = require("./blog.controller.js");
+const {
+  getAllPosts,
+  getPostById,
+  postPost,
+  putPost,
+} = require("./blog.controller.js");
 
 // This how you import stuff from other files. "./" means that we are looking for something inside the same folder as this file (index.js). This is called a relative path,it originates from the file we are currently in.
 let { blogPosts } = require("./data.js");
@@ -20,51 +25,10 @@ app.use(express.json());
 // ########## Endpoints ##########
 
 app.get("/posts", getAllPosts);
-app.get("/blog-posts/:id", (req, res) => {
-  const params = req.params;
-  const id = params.id;
+app.get("/posts/:id", getPostById);
+app.post("/posts", postPost);
+app.put("/posts/:id", putPost);
 
-  // Down below is the destructed alternative to get the id.
-  //   const { id } = req.params;
-
-  // "Find" is an array method, that loops through the array on which it was invoked on. It runs a callback function on each element in the array and does a check. If that check is true, that element will be returned. If every check in every iteration is false, in the end, undefined will be returned.
-  const blog = blogPosts.find((bp) => {
-    if (bp.id === id) {
-      return true;
-    }
-
-    return false;
-  });
-
-  if (blog) {
-    return res.json(blog);
-  }
-
-  return res
-    .status(404) // Means NOT FOUND
-    .json({ message: "The blog with that id was not found" });
-});
-app.post("/posts", postBlog);
-app.put("/blog-posts/:id", (req, res) => {
-  const { id } = req.params;
-  const { content } = req.body;
-
-  if (content === undefined) {
-    return res.status(400).json({ message: "The body is missing." });
-  }
-
-  const blog = blogPosts.find((bp) => bp.id === id);
-
-  if (!blog) {
-    return res
-      .status(404)
-      .json({ message: "The blog with that id was not found" });
-  }
-
-  blog.content = content;
-
-  return res.json(blog);
-});
 app.delete("/blog-posts/:id", (req, res) => {
   const { id } = req.params;
 
